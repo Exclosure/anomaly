2dd969b237c1877a051026b7477dc84ffcaae2e1
Mike McCoy <michael.b.mccoy@gmail.com>
2021-12-16 01:11:17 +0000

    Black.

--- anomaly/optimizers/_custom_root_patch.py
+++ anomaly/optimizers/_custom_root_patch.py
@@ -8,112 +8,118 @@ from jax import linear_util as lu
 from jax.interpreters import ad
 from jax.tree_util import tree_flatten, tree_unflatten, treedef_children, treedef_tuple
 from jax._src.traceback_util import api_boundary
-from jax._src.lax.control_flow import _abstractify, _map, _initial_style_jaxpr, _check_tree, _RootTuple, _flatten, _split_root_args
+from jax._src.lax.control_flow import (
+    _abstractify,
+    _map,
+    _initial_style_jaxpr,
+    _check_tree,
+    _RootTuple,
+    _flatten,
+    _split_root_args,
+)
 
 
 # This is the patch
 def _stop_gradient_fun(f):
-  """This is the only patch."""
-  return f
+    """This is the only patch."""
+    return f
 
 
 @api_boundary
 def custom_root(f, initial_guess, solve, tangent_solve):
-  """Differentiably solve for a roots of a function.
+    """Differentiably solve for a roots of a function.
 
-  This is a low-level routine, mostly intended for internal use in JAX.
-  Gradients of custom_root() are defined with respect to closed-over variables
-  from the provided function ``f`` via the implicit function theorem:
-  https://en.wikipedia.org/wiki/Implicit_function_theorem
+    This is a low-level routine, mostly intended for internal use in JAX.
+    Gradients of custom_root() are defined with respect to closed-over variables
+    from the provided function ``f`` via the implicit function theorem:
+    https://en.wikipedia.org/wiki/Implicit_function_theorem
 
-  Args:
-    f: function for which to find a root. Should accept a single argument,
-      return a tree of arrays with the same structure as its input.
-    initial_guess: initial guess for a zero of f.
-    solve: function to solve for the roots of f. Should take two positional
-      arguments, f and initial_guess, and return a solution with the same
-      structure as initial_guess such that func(solution) = 0. In other words,
-      the following is assumed to be true (but not checked)::
+    Args:
+      f: function for which to find a root. Should accept a single argument,
+        return a tree of arrays with the same structure as its input.
+      initial_guess: initial guess for a zero of f.
+      solve: function to solve for the roots of f. Should take two positional
+        arguments, f and initial_guess, and return a solution with the same
+        structure as initial_guess such that func(solution) = 0. In other words,
+        the following is assumed to be true (but not checked)::
 
-        solution = solve(f, initial_guess)
-        error = f(solution)
-        assert all(error == 0)
+          solution = solve(f, initial_guess)
+          error = f(solution)
+          assert all(error == 0)
 
-    tangent_solve: function to solve the tangent system. Should take two
-      positional arguments, a linear function ``g`` (the function ``f``
-      linearized at its root) and a tree of array(s) ``y`` with the same
-      structure as initial_guess, and return a solution ``x`` such that
-      ``g(x)=y``:
+      tangent_solve: function to solve the tangent system. Should take two
+        positional arguments, a linear function ``g`` (the function ``f``
+        linearized at its root) and a tree of array(s) ``y`` with the same
+        structure as initial_guess, and return a solution ``x`` such that
+        ``g(x)=y``:
 
-      - For scalar ``y``, use ``lambda g, y: y / g(1.0)``.
-      - For vector ``y``, you could use a linear solve with the Jacobian, if
-        dimensionality of ``y`` is not too large:
-        ``lambda g, y: np.linalg.solve(jacobian(g)(y), y)``.
+        - For scalar ``y``, use ``lambda g, y: y / g(1.0)``.
+        - For vector ``y``, you could use a linear solve with the Jacobian, if
+          dimensionality of ``y`` is not too large:
+          ``lambda g, y: np.linalg.solve(jacobian(g)(y), y)``.
 
-  Returns:
-    The result of calling solve(f, initial_guess) with gradients defined via
-    implicit differentiation assuming ``f(solve(f, initial_guess)) == 0``.
-  """
-  guess_flat, in_args_tree = tree_flatten((initial_guess,))
-  guess_avals = tuple(_map(_abstractify, guess_flat))
-  f_jaxpr, f_consts, out_tree = _initial_style_jaxpr(
-      f, in_args_tree, guess_avals)
+    Returns:
+      The result of calling solve(f, initial_guess) with gradients defined via
+      implicit differentiation assuming ``f(solve(f, initial_guess)) == 0``.
+    """
+    guess_flat, in_args_tree = tree_flatten((initial_guess,))
+    guess_avals = tuple(_map(_abstractify, guess_flat))
+    f_jaxpr, f_consts, out_tree = _initial_style_jaxpr(f, in_args_tree, guess_avals)
 
-  in_tree, = treedef_children(in_args_tree)
-  _check_tree("f", "initial_guess", out_tree, in_tree)
+    (in_tree,) = treedef_children(in_args_tree)
+    _check_tree("f", "initial_guess", out_tree, in_tree)
 
-  solve_jaxpr, solve_consts, solution_tree = _initial_style_jaxpr(
-      partial(solve, _stop_gradient_fun(f)), in_args_tree, guess_avals)
-  _check_tree("solve", "initial_guess", solution_tree, in_tree)
+    solve_jaxpr, solve_consts, solution_tree = _initial_style_jaxpr(
+        partial(solve, _stop_gradient_fun(f)), in_args_tree, guess_avals
+    )
+    _check_tree("solve", "initial_guess", solution_tree, in_tree)
 
-  def linearize_and_solve(x, b):
-    unchecked_zeros, f_jvp = jax.linearize(f, x)
-    return tangent_solve(f_jvp, b)
+    def linearize_and_solve(x, b):
+        unchecked_zeros, f_jvp = jax.linearize(f, x)
+        return tangent_solve(f_jvp, b)
 
-  l_and_s_jaxpr, l_and_s_consts, out_tree = _initial_style_jaxpr(
-      linearize_and_solve, treedef_tuple((in_tree,) * 2), guess_avals * 2)
-  _check_tree("tangent_solve", "x", out_tree, in_tree)
+    l_and_s_jaxpr, l_and_s_consts, out_tree = _initial_style_jaxpr(
+        linearize_and_solve, treedef_tuple((in_tree,) * 2), guess_avals * 2
+    )
+    _check_tree("tangent_solve", "x", out_tree, in_tree)
 
-  all_consts = [f_consts, solve_consts, l_and_s_consts]
-  const_lengths = _RootTuple(*_map(len, all_consts))
-  jaxprs = _RootTuple(f_jaxpr, solve_jaxpr, l_and_s_jaxpr)
+    all_consts = [f_consts, solve_consts, l_and_s_consts]
+    const_lengths = _RootTuple(*_map(len, all_consts))
+    jaxprs = _RootTuple(f_jaxpr, solve_jaxpr, l_and_s_jaxpr)
 
-  out_flat = _custom_root(
-      const_lengths, jaxprs, *(_flatten(all_consts) + guess_flat))
-  return tree_unflatten(out_tree, out_flat)
+    out_flat = _custom_root(const_lengths, jaxprs, *(_flatten(all_consts) + guess_flat))
+    return tree_unflatten(out_tree, out_flat)
 
 
 @partial(jax.custom_jvp, nondiff_argnums=(0, 1))
 def _custom_root(const_lengths, jaxprs, *args):
-  params, initial_guess = _split_root_args(args, const_lengths)
-  solution = core.jaxpr_as_fun(jaxprs.solve)(*(params.solve + initial_guess))
-  return solution
+    params, initial_guess = _split_root_args(args, const_lengths)
+    solution = core.jaxpr_as_fun(jaxprs.solve)(*(params.solve + initial_guess))
+    return solution
 
 
 @_custom_root.defjvp
 def _root_jvp(const_lengths, jaxprs, primals, tangents):
-  params, _ = _split_root_args(primals, const_lengths)
-  solution = _custom_root(const_lengths, jaxprs, *primals)
+    params, _ = _split_root_args(primals, const_lengths)
+    solution = _custom_root(const_lengths, jaxprs, *primals)
 
-  params_dot, _ = _split_root_args(tangents, const_lengths)
+    params_dot, _ = _split_root_args(tangents, const_lengths)
 
-  # F(m, u) = 0      # system of equations in u, parameterized by m
-  #                  # solution is u*(m) defined in a neighborhood
-  # F(m, u*(m)) = 0  # satisfied in a neighborhood
-  #
-  # ∂_0 F(m, u*(m)) + ∂_1 F(m, u*(m)) ∂ u*(m) = 0       # implied by line above
-  # ∂ u*(m) = - (∂_1 F(m, u*(m)))^{-1} ∂_0 F(m, u*(m))  # rearrange
-  #
-  # ∂ u*(m)[v] = - (∂_1 F(m, u*(m)))^{-1} [∂_0 F(m, u*(m))[v]]  # jvp
+    # F(m, u) = 0      # system of equations in u, parameterized by m
+    #                  # solution is u*(m) defined in a neighborhood
+    # F(m, u*(m)) = 0  # satisfied in a neighborhood
+    #
+    # ∂_0 F(m, u*(m)) + ∂_1 F(m, u*(m)) ∂ u*(m) = 0       # implied by line above
+    # ∂ u*(m) = - (∂_1 F(m, u*(m)))^{-1} ∂_0 F(m, u*(m))  # rearrange
+    #
+    # ∂ u*(m)[v] = - (∂_1 F(m, u*(m)))^{-1} [∂_0 F(m, u*(m))[v]]  # jvp
 
-  f = core.jaxpr_as_fun(jaxprs.f)
-  linearize_and_solve = partial(
-      core.jaxpr_as_fun(jaxprs.l_and_s), *params.l_and_s)
-  f_at_solution = lambda *params: f(*itertools.chain(params, solution))
-  _, rhs = ad.jvp(lu.wrap_init(f_at_solution)).call_wrapped(
-      params.f, params_dot.f)
-  solution_dot = _map(
-      operator.neg, linearize_and_solve(*itertools.chain(solution, rhs)))
+    f = core.jaxpr_as_fun(jaxprs.f)
+    linearize_and_solve = partial(core.jaxpr_as_fun(jaxprs.l_and_s), *params.l_and_s)
+    f_at_solution = lambda *params: f(*itertools.chain(params, solution))
+    _, rhs = ad.jvp(lu.wrap_init(f_at_solution)).call_wrapped(params.f, params_dot.f)
+    solution_dot = _map(
+        operator.neg, linearize_and_solve(*itertools.chain(solution, rhs))
+    )
 
-  return solution, solution_dot
-
+    return solution, solution_dot
--- test/anomaly/optimizers/test_newton.py
+++ test/anomaly/optimizers/test_newton.py
@@ -12,115 +12,124 @@ from jax import config
 
 
 def one_dim_test(x: jnp.ndarray) -> jnp.ndarray:
-  # Has roots at 1, 2, -2
-  return jnp.power(2*x - 2, 3) * jnp.power(jnp.power(x, 2)-4, 2)
+    # Has roots at 1, 2, -2
+    return jnp.power(2 * x - 2, 3) * jnp.power(jnp.power(x, 2) - 4, 2)
+
 
 test_points = jnp.array(
     [
-      [-5.1, -2],
-      [-3, -2],
-      [-2.1, -2],
-      [-1, 2],
-      [0, 1],
-      [0.5, 1],
-      [1.1, 1],
-      [2.2, 2],
-      [3, 2],
+        [-5.1, -2],
+        [-3, -2],
+        [-2.1, -2],
+        [-1, 2],
+        [0, 1],
+        [0.5, 1],
+        [1.1, 1],
+        [2.2, 2],
+        [3, 2],
     ]
-  )
-
-transform = jnp.array(
-  toeplitz([0.9 ** i for i in range(test_points.shape[0])])
 )
+
+transform = jnp.array(toeplitz([0.9 ** i for i in range(test_points.shape[0])]))
 transform_inv = jnp.linalg.inv(transform)
 complex_method = lambda x: transform @ one_dim_test(transform_inv @ x)
 
+
 def test_newton_1d():
-  # Note: The optimization to has trouble
-  # with the flatness near the root at 1,
-  # resulting in lower-quality solutions.
-  optimizer = lambda x0: newton_1d(one_dim_test, x0)
-  for x0,y0 in test_points:
-    result = optimizer(x0)
-    np.testing.assert_almost_equal(result, y0, decimal=5)
+    # Note: The optimization to has trouble
+    # with the flatness near the root at 1,
+    # resulting in lower-quality solutions.
+    optimizer = lambda x0: newton_1d(one_dim_test, x0)
+    for x0, y0 in test_points:
+        result = optimizer(x0)
+        np.testing.assert_almost_equal(result, y0, decimal=5)
 
 
 def test_newton_raphson_diagonal():
-  # This is a diagonal test -- mixed partials are zero
-  result = newton_raphson(one_dim_test, test_points[:, 0])
-  np.testing.assert_array_almost_equal(result, test_points[:, 1])
+    # This is a diagonal test -- mixed partials are zero
+    result = newton_raphson(one_dim_test, test_points[:, 0])
+    np.testing.assert_array_almost_equal(result, test_points[:, 1])
 
 
 def test_newton_raphson_rotated():
-  # Here, we rotate the inputs and outputs in our 1D method
-  # to make a more complex optimization.
+    # Here, we rotate the inputs and outputs in our 1D method
+    # to make a more complex optimization.
 
-  result = newton_raphson(complex_method, test_points[:, 0])
-  np.testing.assert_array_almost_equal(
-    complex_method(result), jnp.zeros((test_points.shape[0],)))
+    result = newton_raphson(complex_method, test_points[:, 0])
+    np.testing.assert_array_almost_equal(
+        complex_method(result), jnp.zeros((test_points.shape[0],))
+    )
 
 
 def test_newton_raphson_vmap():
-  newton_vmap = jax.vmap(
-    lambda f, x0: newton_raphson(f, x0),
-    in_axes=(None, 0,),
-    out_axes=0,
-  )
-  result = newton_vmap(one_dim_test, test_points[:, 0])
-  np.testing.assert_array_almost_equal(result, test_points[:, 1])
+    newton_vmap = jax.vmap(
+        lambda f, x0: newton_raphson(f, x0),
+        in_axes=(
+            None,
+            0,
+        ),
+        out_axes=0,
+    )
+    result = newton_vmap(one_dim_test, test_points[:, 0])
+    np.testing.assert_array_almost_equal(result, test_points[:, 1])
 
 
 def sqrt_cubed_newton(x):
-  implicit = lambda y: y ** 2 - x ** 3
-  # Note: use x as starting point
-  return newton_raphson(implicit, x)
+    implicit = lambda y: y ** 2 - x ** 3
+    # Note: use x as starting point
+    return newton_raphson(implicit, x)
 
 
 def test_newton_derivatives_scalar():
-  x = 5.0
-  v = sqrt_cubed_newton(x)
-  g = jax.jacfwd(sqrt_cubed_newton)(x)
-  np.testing.assert_allclose(v, x ** 1.5)
-  np.testing.assert_allclose(g, 1.5 * x ** 0.5)
+    x = 5.0
+    v = sqrt_cubed_newton(x)
+    g = jax.jacfwd(sqrt_cubed_newton)(x)
+    np.testing.assert_allclose(v, x ** 1.5)
+    np.testing.assert_allclose(g, 1.5 * x ** 0.5)
 
 
 def test_newton_derivatives_scalar_array():
-  x = jnp.array([5.0])
-  v = sqrt_cubed_newton(x)
-  g = jax.jacfwd(sqrt_cubed_newton)(x)
-  np.testing.assert_allclose(v, x ** 1.5)
-  np.testing.assert_allclose(g, 1.5 * x[:, jnp.newaxis] ** 0.5)
+    x = jnp.array([5.0])
+    v = sqrt_cubed_newton(x)
+    g = jax.jacfwd(sqrt_cubed_newton)(x)
+    np.testing.assert_allclose(v, x ** 1.5)
+    np.testing.assert_allclose(g, 1.5 * x[:, jnp.newaxis] ** 0.5)
 
 
 def test_newton_derivatives_vector():
-  x = jnp.array([1.0, 2.0, 3.0, 4.0, 5.0])
-  v = sqrt_cubed_newton(x)
-  g = jax.jacfwd(sqrt_cubed_newton)(x)
-  np.testing.assert_allclose(v, x ** 1.5)
-  np.testing.assert_allclose(g, 1.5 * jnp.diag(x) ** 0.5)
+    x = jnp.array([1.0, 2.0, 3.0, 4.0, 5.0])
+    v = sqrt_cubed_newton(x)
+    g = jax.jacfwd(sqrt_cubed_newton)(x)
+    np.testing.assert_allclose(v, x ** 1.5)
+    np.testing.assert_allclose(g, 1.5 * jnp.diag(x) ** 0.5)
 
 
 def test_newton_derivatives_vmap():
-  x = jnp.array([1.0, 2.0, 3.0, 4.0, 5.0])
-  v = jax.vmap(sqrt_cubed_newton)(x)
-  g = jax.vmap(jax.jacfwd(sqrt_cubed_newton))(x)
-  np.testing.assert_allclose(v, x ** 1.5)
-  np.testing.assert_allclose(g, 1.5 * x ** 0.5)
+    x = jnp.array([1.0, 2.0, 3.0, 4.0, 5.0])
+    v = jax.vmap(sqrt_cubed_newton)(x)
+    g = jax.vmap(jax.jacfwd(sqrt_cubed_newton))(x)
+    np.testing.assert_allclose(v, x ** 1.5)
+    np.testing.assert_allclose(g, 1.5 * x ** 0.5)
 
 
 def test_linear_solve():
-  A, b = jnp.array(1.), jnp.array(1.)
-  np.testing.assert_almost_equal(linear_solve(A, b), 1.0)
+    A, b = jnp.array(1.0), jnp.array(1.0)
+    np.testing.assert_almost_equal(linear_solve(A, b), 1.0)
 
-  A2, b2 = jnp.array([[2., 0.], [0., 1.]]), jnp.array([2., 1.])
-  np.testing.assert_array_almost_equal(linear_solve(A2, b2), jnp.array([1., 1.]))
-  np.testing.assert_array_almost_equal(linear_solve(A2, b2.reshape((-1,1))), jnp.array([[1., 1.]]).T)
+    A2, b2 = jnp.array([[2.0, 0.0], [0.0, 1.0]]), jnp.array([2.0, 1.0])
+    np.testing.assert_array_almost_equal(linear_solve(A2, b2), jnp.array([1.0, 1.0]))
+    np.testing.assert_array_almost_equal(
+        linear_solve(A2, b2.reshape((-1, 1))), jnp.array([[1.0, 1.0]]).T
+    )
 
-  jit_solve = jax.jit(linear_solve)
-  np.testing.assert_array_almost_equal(jit_solve(A2, b2), jnp.array([1., 1.]))
-  np.testing.assert_array_almost_equal(jit_solve(A2, b2.reshape((-1,1))), jnp.array([[1., 1.]]).T)
+    jit_solve = jax.jit(linear_solve)
+    np.testing.assert_array_almost_equal(jit_solve(A2, b2), jnp.array([1.0, 1.0]))
+    np.testing.assert_array_almost_equal(
+        jit_solve(A2, b2.reshape((-1, 1))), jnp.array([[1.0, 1.0]]).T
+    )
 
 
 if __name__ == "__main__":
-  import pytest
-  pytest.main()
+    import pytest
+
+    pytest.main()
